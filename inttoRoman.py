07601150e91b260a6a3126327b30de646415de9b
Lee Kang Houn <48376964+nightandweather@users.noreply.github.com>
2020-12-21 14:41:11 +0900

    Create inttoRoman.py

---
+++ inttoRoman.py
@@ -0,0 +1,8 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        symbol_value = {"M":1000, "CM":900, "D":500, "CD":400, "C":100, "XC":90, "L":50, "XL":40, "X":10, "IX":9, "V":5 , "IV":4 , "I":1}
+        roman = ""
+        for i in symbol_value:
+            roman += i*(num//symbol_value[i])
+            num = num%symbol_value[i]
+        return roman
